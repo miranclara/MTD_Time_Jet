b3583960c4d6192a0826796bfb74210217d60463
miran.kim <mi.ran.kim@cern.ch>
2025-05-30 15:10:06 +0200

    Add Crab configuration

--- JetTreeProducer/test/myTest_cfg.py
+++ JetTreeProducer/test/myTest_cfg.py
@@ -7,23 +7,24 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 # Input source 
 process.source = cms.Source("PoolSource",
-#    fileNames = cms.untracked.vstring('file:patTuple_onlyJets.root')
+    fileNames = cms.untracked.vstring()
 #    fileNames = cms.untracked.vstring('/store/relval/CMSSW_15_0_0/RelValTTbar_14TeV/MINIAODSIM/PU_141X_mcRun4_realistic_v3_STD_Run4D110_PU-v2/2580000/9c9737ed-bb08-48c5-9f58-ba4bb149c0d7.root')
-    fileNames = cms.untracked.vstring(
-        "root://cms-xrd-global.cern.ch//store/mc/Phase2HLTTDRSummer20ReRECOMiniAOD/"
-        "QCD_Pt_300to470_TuneCP5_14TeV_pythia8/GEN-SIM-DIGI-RAW-MINIAOD/"
-        "NoPU_111X_mcRun4_realistic_T15_v1-v1/100000/"
-        "E4BCEE62-D6E4-114B-88C3-E3FF47AAA099.root"),
-    redirectorHints = cms.untracked.vstring(
-        "root://cms-xrd-global.cern.ch/"
-    )    
+#    fileNames = cms.untracked.vstring('/store/mc/Phase2HLTTDRSummer20ReRECOMiniAOD/QCD_Pt_300to470_TuneCP5_14TeV_pythia8/GEN-SIM-DIGI-RAW-MINIAOD/NoPU_111X_mcRun4_realistic_T15_v1-v1/100000/1750775B-F6D7-AE4A-8097-AE0181CE03A9.root')
+#    fileNames = cms.untracked.vstring(
+#        "root://cms-xrd-global.cern.ch//store/mc/Phase2HLTTDRSummer20ReRECOMiniAOD/"
+#        "QCD_Pt_300to470_TuneCP5_14TeV_pythia8/GEN-SIM-DIGI-RAW-MINIAOD/"
+#        "NoPU_111X_mcRun4_realistic_T15_v1-v1/100000/"
+#        "E4BCEE62-D6E4-114B-88C3-E3FF47AAA099.root"),
+#    redirectorHints = cms.untracked.vstring(
+#        "root://cms-xrd-global.cern.ch/"
+#    )    
 )
 #when switching between AOD and MINIAOD
 #process.options.allowUnscheduled = cms.untracked.bool(True)
 
 # Max events
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)  # process all events(-1)
+    input = cms.untracked.int32(-1)  # process all events(-1)
 )
 
 # iJetTreeProducer configuration
